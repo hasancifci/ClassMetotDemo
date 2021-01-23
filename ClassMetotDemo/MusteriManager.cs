4dee398f935e64caedc116237586895d205873b6
hasancifciiHasan <hasancifcii@outlook.com>
2021-01-23 16:38:32 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Musteri Eklendi " + musteri.Adi);
+        }
+
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("Musteri Silindi " + musteri.Adi);
+        }
+        public void Listele(Musteri musteri)
+        {
+            Console.WriteLine("Musteri Listelendi " + musteri.Adi);
+        }
+        public void Güncelle(Musteri musteri)
+        {
+            Console.WriteLine("Musteri Güncellendi " + musteri.Adi);
+        }
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Adi = "Hasan";
+            musteri1.Soyadi = "Çifçi";
+            musteri1.Aciklama = "Açıklama Eklendi";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Adi = "Ece Yaren";
+            musteri2.Soyadi = "Taykutgül";
+            musteri2.Aciklama = "Açıklama Eklendi";
+
+            MusteriManager musteriManager = new MusteriManager();
+
+            musteriManager.Ekle(musteri1);
+            musteriManager.Listele(musteri1);
+            musteriManager.Sil(musteri1);
+            musteriManager.Güncelle(musteri2);
+
+        }
+    }
+}
